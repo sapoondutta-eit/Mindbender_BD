16990a8b67e6dd47387b231dae98a32f0853f4f9
sapoondutta-eit <sapoonjyoti.duttaduwarah@enhanceit.us>
2020-11-04 12:31:06 -0500

    added an option for task3

--- task3/start_hadoop.sh
+++ task3/start_hadoop.sh
@@ -1,13 +1,32 @@
 #!usr/bin/bash
 
-OUTPUT=$(jps)
-echo "${OUTPUT}"
 
-if [[ $OUTPUT == *"ResourceManager"* ]]
-then
-   echo "It's there!"
-else 
-  eval "start-all.sh"
+echo "Enter 1 to start serivce"
+echo "Enter 2 to stop servie"
+read opt 
+
+if [[ $opt == 1 ]]; then
+	OUTPUT=$(jps)
+	echo "${OUTPUT}"
+
+	if [[ $OUTPUT == *"ResourceManager"* ]]
+		then
+   		echo "It's already running!"
+	else 
+  		eval "start-all.sh"
+	fi
+else
+	OUTPUT=$(jps)
+	echo "${OUTPUT}"
+
+	if [[ $OUTPUT == *"ResourceManager"* ]]
+		then
+   		echo "It's running!"
+		eval "stop-all.sh"
+		
+	else 
+  		echo "it's already stopped"
+	fi
 fi
 
 #$java_check = jps
