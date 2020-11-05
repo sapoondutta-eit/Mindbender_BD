67d1d56ac92403fb9211160c09ac6b3c27e439fb
sapoondutta-eit <sapoonjyoti.duttaduwarah@enhanceit.us>
2020-11-05 08:31:00 -0500

    updated task4

--- task4/install_hadoop.sh
+++ task4/install_hadoop.sh
@@ -10,24 +10,33 @@ rm hadoop-2.7.3.tar.gz
 
 cd 
 nano bash_profile.sh
-echo 'export JAVA_HOME=~/opt/jdk1.8.0_221' > ./bash_profile.sh
-echo 'export PATH=$PATH:$JAVA_HOME/bin' > ./bash_profile.sh
-echo 'export HADOOP_HOME= ~/opt/hadoop-2.7.3' > ./bash_profile.sh
-echo 'export HADOOP_INSTALL=$HADOOP_HOME' > ./bash_profile.sh
-echo 'export HADOOP_MAPRED_HOME=$HADOOP_HOME' > ./bash_profile.sh
-echo 'export HADOOP_COMMON_HOME=$HADOOP_HOME' > ./bash_profile.sh
-echo 'export HADOOP_HDFS_HOME=$HADOOP_HOME' > ./bash_profile.sh
-echo 'export YARN_HOME=$HADOOP_HOME' > ./bash_profile.sh
-echo 'export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native' > ./bash_profile.sh
-echo 'export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin' > ./bash_profile.sh
+echo 'export JAVA_HOME=~/opt/jdk1.8.0_221' >> ./bash_profile.sh
+echo 'export PATH=$PATH:$JAVA_HOME/bin' >> ./bash_profile.sh
+echo 'export HADOOP_HOME= ~/opt/hadoop-2.7.3' >> ./bash_profile.sh
+echo 'export HADOOP_INSTALL=$HADOOP_HOME' >> ./bash_profile.sh
+echo 'export HADOOP_MAPRED_HOME=$HADOOP_HOME' >> ./bash_profile.sh
+echo 'export HADOOP_COMMON_HOME=$HADOOP_HOME' >> ./bash_profile.sh
+echo 'export HADOOP_HDFS_HOME=$HADOOP_HOME' >> ./bash_profile.sh
+echo 'export YARN_HOME=$HADOOP_HOME' >> ./bash_profile.sh
+echo 'export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native' >> ./bash_profile.sh
+echo 'export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin' >> ./bash_profile.sh
 
 
 cd home/marcus/opt/hadoop-2.7.3/etc/hadoop
 
 
-echo 'export JAVA_HOME=/home/deward/opt/jdk1.8.0_221' > hadoop-env.sh
+echo 'export JAVA_HOME=~/opt/jdk1.8.0_221' >> hadoop-env.sh
+
+sed -i 's/<configuration>/ g' core-site.xml
+sed -i 's#/</configuration>/# #g'core-site.xml
+
 
 echo '<configuration> <property> <name>fs.default.name</name> <value>hdfs://localhost:9000</value> </property> </configuration>' > core-site.xml
+
+vsed -i 's/<configuration>/ g' hdfs-site.xml
+sed -i 's#/</configuration>/# #g'hdfs-site.xml
+
+
 echo '<configuration>
 
 	<property>
@@ -38,15 +47,18 @@ echo '<configuration>
 
 	<property>
 		<name>dfs.name.dir</name>
-		<value>file:///home/deward/opt/hadoop-2.7.3/hdfs/namenode</value>
+		<value>file:///~/opt/hadoop-2.7.3/hdfs/namenode</value>
 	</property>
 
 	<property>
 		<name>dfs.name.dir</name>
-		<value>file:///home/deward/opt/hadoop-2.7.3/hdfs/datanode</value>
+		<value>file:///~/opt/hadoop-2.7.3/hdfs/datanode</value>
 	</property>
 	
-</configuration> ' > hdfs-site.xml
+</configuration> ' >> hdfs-site.xml
+
+sed -i 's/<configuration>/ g' yarn-site.xml
+sed -i 's#/</configuration>/# #g'yarn-site.xml
 
 
 echo '<configuration>
@@ -55,7 +67,7 @@ echo '<configuration>
 		<value>mapreduce_shuffle</value>
 	</property>
 
-</configuration>' > yarn-site.xml
+</configuration>' >> yarn-site.xml
 
 cd ../../../../
 chmod 777 opt/
@@ -70,6 +82,10 @@ mkdir datanode
 mkdir namenode
 
 cd ../etc/hadoop/
+
+sed -i 's/<configuration>/ g' hdfs-site.xml
+sed -i 's#/</configuration>/# #g'hdfs-site.xml
+
 echo '<configuration>
 	<property>
 		<name>dfs.replication</name>
@@ -78,35 +94,30 @@ echo '<configuration>
 
 	<property>
 		<name>dfs.name.dir</name>
-		<value>file:///home/user/opt/hadoop-2.7.3/hdfs/namenode</value>
+		<value>file:///~/opt/hadoop-2.7.3/hdfs/namenode</value>
 	</property>
 
 	<property>
 		<name>dfs.name.dir</name>
-		<value>file:///home/user/opt/hadoop-2.7.3/hdfs/datanode</value>
+		<value>file:///~/opt/hadoop-2.7.3/hdfs/datanode</value>
 	</property>
 </configuration>
-' > hdfs-site.xml
+' >> hdfs-site.xml
 
 cp mapred-site.xml.template mapred-site.xml
+
+sed -i 's/<configuration>/ g' mapred-site.xml
+sed -i 's#/</configuration>/# #g'mapred-site.xml
+
 
 echo '<configuration>
 	<property>
 		<name>mapreduce.framework.name</name>
 		<value>yarn</value>
 	</property>
-</configuration>' > mapred-site.xml
+</configuration>' >> mapred-site.xml
 
 
-echo '<configuration>
-
-<!-- Site specific YARN configuration properties -->
-	<property>
-		<name>yarn.nodemanager.aux-services</name>
-		<value>mapreduce_shuffle</value>
-	</property>
-
-</configuration>' > yarn-site.xml
 
 cd 
 
