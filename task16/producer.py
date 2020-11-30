07d0e33972ca12fdfb01168eb5dfa41aed80c56a
sapoondutta-eit <sapoonjyoti.duttaduwarah@enhanceit.us>
2020-11-30 08:51:29 -0500

    single broker producer

---
+++ task16/producer.py
@@ -0,0 +1,25 @@
+import requests
+from kafka import SimpleProducer
+from kafka import KafkaClient
+
+
+TOPIC = "stockmarket"
+
+
+url = "https://twelve-data1.p.rapidapi.com/rsi"
+
+querystring = {"symbol":"FCEL","interval":"1min","series_type":"close","outputsize":"30","format":"json","time_period":"14"}
+
+headers = {
+    'x-rapidapi-key': "91c9731234mshaa0159144ccc387p15ba78jsn35630e0f0930",
+    'x-rapidapi-host': "twelve-data1.p.rapidapi.com"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+RSI = response.text
+
+kafka = KafkaClient("localhost:9099")
+producer = SimpleProducer(kafka)
+
+producer.send_messages(TOPIC, RSI.encode('utf-8'))
